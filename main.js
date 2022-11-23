e39e1a292075ef431bbd910801b3039c51ff64be
JustProdigalSon <ksaweryboroszko@gmail.com>
2022-11-24 00:52:11 +0100

    email stuff

--- main.js
+++ main.js
@@ -17,3 +17,21 @@ const observer = new IntersectionObserver(function(entries,observer) {
 }, options);
 
 observer.observe(sectionAbout)
+
+
+const span = document.querySelector("span");
+
+span.onclick = function() {
+  document.execCommand("copy");
+}
+
+span.addEventListener("copy", function(event) {
+  event.preventDefault();
+  if (event.clipboardData) {
+    event.clipboardData.setData("text/plain", span.textContent);
+    console.log(event.clipboardData.getData("text"))
+  }
+});
+
+
+
