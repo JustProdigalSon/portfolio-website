26d1701b8c7248633b3b481f2ec0459293f43762
JustProdigalSon <ksaweryboroszko@gmail.com>
2022-11-24 00:27:55 +0100

    js stuff

--- main.js
+++ main.js
@@ -2,7 +2,7 @@ const sectionAbout = document.querySelector('.About');
 
 const options = {
     threshold: 1,
-    rootMargin: "0px 0px -200px 0px",
+    rootMargin: "0px 0px -100px 0px",
     threshold: 0.5,
 };
 
